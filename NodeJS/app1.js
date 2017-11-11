0f2eb15e07e1902a0fa59514ee2be800b16f91bc
qcharria <quentin_charriau@orange.fr>
2017-11-11 13:12:46 +0100

    Changement de l'app par une todolist.

--- NodeJS/app1.js
+++ NodeJS/app1.js
@@ -1,22 +1,51 @@
-﻿var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
+﻿var express = require('express');
+var session = require('cookie-session'); // Charge le middleware de sessions
+var bodyParser = require('body-parser'); // Charge le middleware de gestion des paramètres
 
-var server = http.createServer(function(req, res) {
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200, {"Content-Type": "text/plain"});
-	if('prenom' in params && 'nom' in params){
-		res.write('Vous vous appelez ' + params['prenom'] + '' + params['nom'] + ' ! Je vous connais bien maintenant.');
-	}
-    else if ('prenom' in params) {
-        res.write('Bonjour ' + params['prenom'] + ', vous avez l\'air amical !');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+var app = express();
+
+/* On utilise les sessions */
+app.use(session({secret: 'todotopsecret'}))
+/* Le paramètre secret envoyé au module de session est obligatoire : il permet de sécuriser les cookies de session */
+
+/* S'il n'y a pas de todolist dans la session,
+on en crée une vide sous forme d'array avant la suite */
+.use(function(req, res, next){
+    if (typeof(req.session.todolist) == 'undefined') {
+        req.session.todolist = [];
     }
-	else if('nom' in params){
-		res.write('Encore un ' + params['nom'] + ', ils se ressemblent tous pour moi.');
-	}
-    else {
-        res.write('Je ne vous connais pas du tout...');
+    next();
+})
+
+/* On affiche la todolist et le formulaire */
+
+.get('/todo', function(req, res) { 
+    res.render('todo.ejs', {todolist: req.session.todolist});
+})
+
+/* On ajoute un élément à la todolist */
+
+.post('/todo/ajouter/', urlencodedParser, function(req, res) {
+    if (req.body.newtodo != '') {
+        req.session.todolist.push(req.body.newtodo);
     }
-    res.end();
-});
-server.listen(8080);
+    res.redirect('/todo');
+})
+
+/* Supprime un élément de la todolist */
+.get('/todo/supprimer/:id', function(req, res) {
+    if (req.params.id != '') {
+        req.session.todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
+/* On redirige vers la todolist si la page demandée n'est pas trouvée */
+
+.use(function(req, res, next){
+    res.redirect('/todo');
+})
+
+.listen(8080);
